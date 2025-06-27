eb612f321b0a358771d20c2feaf46003a43aa188
Alexander Sinelnik <ssinelnik@bk.ru>
2025-06-27 22:40:56 +0500

    perf(logger): logging correction

--- config.ru
+++ config.ru
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 require_relative 'middleware/runtime'
 require_relative 'middleware/logger'
+require_relative 'lib/simpler'
 require_relative 'config/environment'
 
-run Simpler.application
+Simpler.application.bootstrap!
+
 use AppLogger, logdev: File.expand_path('log/app.log', __dir__)
+
+run Simpler.application
--- middleware/logger.rb
+++ middleware/logger.rb
@@ -2,16 +2,49 @@
 
 require 'logger'
 require 'json'
+require 'rack/utils'
 
 # Rack middleware for HTTP request logging
 class AppLogger
   def initialize(app, **options)
-    @logger = Logger.new(options[:logdev] || STDOUT)
     @app = app
+    @logger = Logger.new(options[:logdev] || STDOUT)
+    @logger.formatter = proc { |severity, datetime, progname, msg| "#{msg}\n" }
   end
 
   def call(env)
-    @logger.info("#{env['REQUEST_METHOD']} #{env['PATH_INFO']} - #{Time.now}")
-    @app.call(env)
+    request = Rack::Request.new(env)
+    http_method = request.request_method
+    full_path = request.fullpath
+    params_hash = request.params
+
+    # @logger.info("#{env['REQUEST_METHOD']} #{env['PATH_INFO']} - #{Time.now}")
+    # @app.call(env)
+
+    route = Simpler.application.send(:instance_variable_get, :@router)
+                       .route_for(env)
+    handler_name = if route
+                       "#{route.controller.name}##{route.action}"
+                     else
+                       "Unknown Handler"
+                     end
+
+    @logger.info("Request: #{http_method} #{full_path}")
+    @logger.info("Handler: #{handler_name}")
+    @logger.info("Parameters: #{params_hash.inspect}")
+
+    status, headers, body = @app.call(env)
+
+    @logger.info("Request: #{http_method} #{full_path}")
+    @logger.info("Handler: #{handler_name}")
+    @logger.info("Parameters: #{params_hash.inspect}")
+
+    status, headers, body = @app.call(env)
+
+    @logger.info("Response: #{status} [#{content_type}]#{template_file ? " #{template_file}" : ''}")
+    # @logger.info("Response: #{status}")
+
+
+    [status, headers, body]
   end
 end
