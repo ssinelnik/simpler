d4524f45cb8ada1416fc173ac2212b022589cc10
Alexander Sinelnik <ssinelnik@bk.ru>
2025-06-27 13:27:06 +0500

    feat(tests_controller): adding the response status and new method

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,6 +3,12 @@
 # A controller for working with tests
 class TestsController < Simpler::Controller
   def index
+    status 201
     render plain: "Plain text response"
+  end
+
+  def not_found
+    status 404
+    render plain: "Resource not found"
   end
 end
